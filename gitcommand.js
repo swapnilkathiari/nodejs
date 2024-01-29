92d2aab20fcb01faef507f5ad21ae6626d8d4289
swapnilkathiari <gulzarkibaateh@gmail.com>
2024-01-29 22:36:34 +0530

    git command file

---
+++ gitcommand.js
@@ -0,0 +1,96 @@
+
+// swapn@Swapnil MINGW64 ~/OneDrive/Desktop/node js
+// $ git config --global user.name 'swapnilkathiari'
+
+// swapn@Swapnil MINGW64 ~/OneDrive/Desktop/node js
+// $ git config --global user.email 'gulzarkibaateh@gmail.com'
+
+// swapn@Swapnil MINGW64 ~/OneDrive/Desktop/node js
+// $ git init
+// Initialized empty Git repository in C:/Users/swapn/OneDrive/Desktop/node js/.git/
+
+// swapn@Swapnil MINGW64 ~/OneDrive/Desktop/node js (master)
+// $ git status
+// On branch master
+
+// No commits yet
+
+// Untracked files:
+//   (use "git add <file>..." to include in what will be committed)
+//         a.js
+//         arrowfunction.js
+//         employee.js
+//         globalobj.js
+//         object.js
+//         teacher.js
+//         user.js
+
+// nothing added to commit but untracked files present (use "git add" to track)
+
+// swapn@Swapnil MINGW64 ~/OneDrive/Desktop/node js (master)
+// $ git add .
+
+// swapn@Swapnil MINGW64 ~/OneDrive/Desktop/node js (master)
+// $ git status
+// On branch master
+
+// No commits yet
+
+// Changes to be committed:
+//   (use "git rm --cached <file>..." to unstage)
+//         new file:   a.js
+//         new file:   arrowfunction.js
+//         new file:   employee.js
+//         new file:   globalobj.js
+//         new file:   object.js
+//         new file:   teacher.js
+//         new file:   user.js
+
+
+// swapn@Swapnil MINGW64 ~/OneDrive/Desktop/node js (master)
+// $ git commit -m 'node js files'
+// [master (root-commit) 591fc47] node js files
+//  7 files changed, 69 insertions(+)
+//  create mode 100644 a.js
+//  create mode 100644 arrowfunction.js
+//  create mode 100644 employee.js
+//  create mode 100644 globalobj.js
+//  create mode 100644 object.js
+//  create mode 100644 teacher.js
+//  create mode 100644 user.js
+
+// swapn@Swapnil MINGW64 ~/OneDrive/Desktop/node js (master)
+// $ git branch -M main
+
+// swapn@Swapnil MINGW64 ~/OneDrive/Desktop/node js (main)
+// $ git git remote add origin https://github.com/swapnilkathiari/nodejs.git
+// git: 'git' is not a git command. See 'git --help'.
+
+// The most similar command is
+//         init
+
+// swapn@Swapnil MINGW64 ~/OneDrive/Desktop/node js (main)
+// $ git remote add origin https://github.com/swapnilkathiari/nodejs.git
+
+// swapn@Swapnil MINGW64 ~/OneDrive/Desktop/node js (main)
+// $ git push -u origin main
+// Enumerating objects: 9, done.
+// Counting objects: 100% (9/9), done.
+// Delta compression using up to 12 threads
+// Compressing objects: 100% (8/8), done.
+// Writing objects: 100% (9/9), 1.12 KiB | 114.00 KiB/s, done.
+// Total 9 (delta 0), reused 0 (delta 0), pack-reused 0
+// To https://github.com/swapnilkathiari/nodejs.git
+//  * [new branch]      main -> main
+// branch 'main' set up to track 'origin/main'.
+
+// swapn@Swapnil MINGW64 ~/OneDrive/Desktop/node js (main)
+// $ git push
+
+// Everything up-to-date
+
+// swapn@Swapnil MINGW64 ~/OneDrive/Desktop/node js (main)
+// $ node^C
+
+// swapn@Swapnil MINGW64 ~/OneDrive/Desktop/node js (main)
+// $
